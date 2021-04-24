a002aff50ad16b60448f4315ba697337db8dbab1
BangtanAi <kairova-94@mail.ru>
2021-04-25 01:39:45 +0600

    5. крутилка для стрелки SortPopup, react-router подключен, link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import logoSvg from './../assets/img/pizza-logo.svg';
 import Button from './Button';
+import { Link } from 'react-router-dom';
 
-function Header() {        
-    return (
-        <div className="header">
-        <div className="container">
+function Header() {
+  return (
+    <div className="header">
+      <div className="container">
+        <Link to="/">
           <div className="header__logo">
-            <img width="38" src={logoSvg} alt='Pizza logo' />
+            <img width="38" src={logoSvg} alt="Pizza logo" />
             <div>
               <h1>React Pizza</h1>
               <p>самая вкусная пицца во вселенной</p>
             </div>
           </div>
-          <div className="header__cart">
+        </Link>
+
+        <div className="header__cart">
+          <Link to="/cart">
             <Button className="button--cart">
               <span>520 ₽</span>
               <div className="button__delimiter"></div>
@@ -22,8 +27,7 @@ function Header() {
                 height="18"
                 viewBox="0 0 18 18"
                 fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+                xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M6.33333 16.3333C7.06971 16.3333 7.66667 15.7364 7.66667 15C7.66667 14.2636 7.06971 13.6667 6.33333 13.6667C5.59695 13.6667 5 14.2636 5 15C5 15.7364 5.59695 16.3333 6.33333 16.3333Z"
                   stroke="white"
@@ -48,10 +52,11 @@ function Header() {
               </svg>
               <span>3</span>
             </Button>
-          </div>
+          </Link>
         </div>
       </div>
-    )
+    </div>
+  );
 }
 
 export default Header;
