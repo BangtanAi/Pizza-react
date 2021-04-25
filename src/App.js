fa6d6c9ac224642d7517c6320a3b34a466038c12
BangtanAi <kairova-94@mail.ru>
2021-04-25 15:55:50 +0600

    6. Берем все пиццы и инфу о них из db.json, делаем элементы активными или неактивными с помощью useState

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ import Home from './pages/Home';
 
 
 function App() {
+  const[pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' render={() => <Home items={pizzas} /> } />
         <Route exact path='/cart' component={Cart} />
       </div>
     </div>
