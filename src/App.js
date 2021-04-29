a56e5e467df3824c695e621329027d0fc6b29a71
BangtanAi <kairova-94@mail.ru>
2021-04-30 01:19:44 +0600

    9. урок #6 пройден, redux, react-redux подключены, пока App на классах, mdtp, mstp, connect, Provider, actionCreator, reducer

--- src/App.js
+++ src/App.js
@@ -4,27 +4,56 @@ import { Route } from 'react-router-dom';
 import Header from './components/Header';
 import Cart from './pages/Cart';
 import Home from './pages/Home';
+import store from './redux/store';
+import { setPizzas } from './redux/actions/pizzas';
+import { connect } from 'react-redux';
 
-
-
-function App() {
-  const[pizzas, setPizzas] = React.useState([]);
-
-  React.useEffect(() => {
+class App extends React.Component {
+  componentDidMount() {
     axios.get('http://localhost:3000/db.json').then(({data}) => {
-      setPizzas(data.pizzas);
-    })
-  }, []);
-
-  return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Route exact path='/' render={() => <Home items={pizzas} /> } />
-        <Route exact path='/cart' component={Cart} />
+       this.props.setPizzas(data.pizzas);
+     });
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <Route exact path='/' render={() => <Home items={this.props.items} /> } />
+          <Route exact path='/cart' component={Cart} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+// function App() {
+//   const[pizzas, setPizzas] = React.useState([]);
+
+//   React.useEffect(() => {
+//     axios.get('http://localhost:3000/db.json').then(({data}) => {
+//       setPizzas(data.pizzas);
+//     })
+//   }, []);
+
+//   return (
+//     <div className="wrapper">
+//       <Header />
+//       <div className="content">
+//         <Route exact path='/' render={() => <Home items={pizzas} /> } />
+//         <Route exact path='/cart' component={Cart} />
+//       </div>
+//     </div>
+//   );
+// }
+
+const mapStateToProps=(state) => {
+  return {
+    items: state.pizzas.items,
+  };
+};
+const mapDispatchToProps = {
+  setPizzas,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -4,7 +4,7 @@ function SortPopup({items}) {
     const[visiblePopup, setVisiblePopup] = React.useState(false);
     const[activeItem, setActiveItem] = React.useState(0);
     const sortRef = React.useRef();
-    const activeLabel = items[activeItem];
+    const activeLabel = items[activeItem].name;
 
     const onSelectItem = (index) => {
         setActiveItem(index);
@@ -47,12 +47,12 @@ function SortPopup({items}) {
               </div>
               {visiblePopup && <div className="sort__popup">
                 <ul>
-                {items && items.map((name, index) => (
+                {items && items.map((obj, index) => (
                 <li 
                  className={activeItem === index ? 'active' : ''}
                  onClick={()=> onSelectItem(index)} 
-                 key={`${name}_${index}`}>
-                 {name}
+                 key={`${obj.type}_${index}`}>
+                 {obj.name}
                  </li>))}
                 </ul>
               </div>}
